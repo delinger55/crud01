71614b24ed21618516a29b89db4cba725a66ac7d
Enzo Rojas <enzorojas21@gmail.com>
2024-11-07 10:16:42 -0300

    agregar formulario

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
-    class meta:
+    class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'stock']
